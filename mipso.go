a384a7b4f5500ab7995790503c5b5bdb34b279fb
James Bornholt <bornholt@cs.washington.edu>
2014-12-28 13:14:34 +1100

    a dumb lexer that doesn't work and i will delete
    go is too smart for its own good

--- mipso.go
+++ mipso.go
@@ -1,8 +1,19 @@
 package main
 
 import "fmt"
+import "github.com/jamesbornholt/mipso/assembler"
 
 func main() {
 	v := emit_add_3(0, 1, 2)
 	fmt.Printf("%v\n", v)
+
+    foo := assembler.LexFromFile("/Users/bornholt/code/mipso/test.s")
+    for {
+        switch t := foo.NextToken(); {
+        case t.Typ == assembler.TokEOF:
+            break
+        default:
+            fmt.Printf("%v\n", t);
+        }
+    }
 }
